c25ea5f44cee2ea9f1c0842c5a9c507761b6041a
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-07-10 21:36:28 +0300

    fixed NavMenu

--- src/pages/pageHome/components/NavMenu.jsx
+++ src/pages/pageHome/components/NavMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
